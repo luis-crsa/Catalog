dab7e2841f8d7b06150f9825e250d2fc1eeea53f
Luis <luiscrsarmento@gmail.com>
2024-12-22 17:27:46 -0300

    GET category by id

--- backend/src/main/java/com/luiscrsarmento/catalog/services/CategoryService.java
+++ backend/src/main/java/com/luiscrsarmento/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.luiscrsarmento.catalog.dto.CategoryDTO;
 import com.luiscrsarmento.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +14,13 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         return repository.findAll().stream().map(CategoryDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        return new CategoryDTO(repository.findById(id).get());
     }
 }
